65a1cd6c8a8eb236bdca7e446cb0b66f309ce371
Ratmir Aznabaev <r.aznarbaev@nekki.com>
2025-11-18 00:06:20 +0300

    layout helper for resolutions

---
+++ Assets/Scripts/UI/LayoutHelper.cs
@@ -0,0 +1,36 @@
+using UnityEditor;
+using UnityEngine;
+
+public static class LayoutHelper
+{
+    [MenuItem("uGUI/Anchors to Corners %[")]
+    private static void AnchorsToCorners()
+    {
+        if (Selection.transforms == null || Selection.transforms.Length == 0)
+        {
+            return;
+        }
+        Undo.IncrementCurrentGroup();
+        Undo.SetCurrentGroupName("AnchorsToCorners");
+        var undoGroup = Undo.GetCurrentGroup();
+
+        foreach (Transform transform in Selection.transforms)
+        {
+            RectTransform t = transform as RectTransform;
+            Undo.RecordObject( t, "AnchorsToCorners" );
+            RectTransform pt = Selection.activeTransform.parent as RectTransform;
+
+            if (t == null || pt == null) return;
+
+            Vector2 newAnchorsMin = new Vector2(t.anchorMin.x + t.offsetMin.x / pt.rect.width,
+                t.anchorMin.y + t.offsetMin.y / pt.rect.height);
+            Vector2 newAnchorsMax = new Vector2(t.anchorMax.x + t.offsetMax.x / pt.rect.width,
+                t.anchorMax.y + t.offsetMax.y / pt.rect.height);
+
+            t.anchorMin = newAnchorsMin;
+            t.anchorMax = newAnchorsMax;
+            t.offsetMin = t.offsetMax = new Vector2(0, 0);
+        }
+        Undo.CollapseUndoOperations(undoGroup);
+    }
+}
